c157d4d95c33a6b9f84966901c620162a3ac5c9a
NeuralNotW0rk <griffinpage@gmail.com>
2025-08-04 19:32:09 -0700

    Automatic symlink creation during setup

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,3 +1,9 @@
+src_path=$(pwd)/src
+reaper_resource_path=~/Library/Application\ Support/REAPER
+
+ln -sf "$src_path/Scripts/composers_assistant_v2" "$reaper_resource_path/Scripts"
+ln -sf "$src_path/Effects/composers_assistant_v2" "$reaper_resource_path/Effects"
+
 rm -r .venv
 python3.9 -m venv .venv
 source .venv/bin/activate
