213862361aa1db0c69f8bdb47ddc0b7ff6282ea8
NeuralNotW0rk <griffinpage@gmail.com>
2025-08-07 15:53:45 -0700

    mac setup changes

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,12 +1,14 @@
-src_path=$(pwd)/src
+root_path=$(pwd)
 reaper_resource_path=~/Library/Application\ Support/REAPER
 
-ln -sf "$src_path/Scripts/composers_assistant_v2" "$reaper_resource_path/Scripts"
-ln -sf "$src_path/Effects/composers_assistant_v2" "$reaper_resource_path/Effects"
+ln -sf "$root_path/src/Scripts/composers_assistant_v2" "$reaper_resource_path/Scripts"
+ln -sf "$root_path/src/Effects/composers_assistant_v2" "$reaper_resource_path/Effects"
 
-rm -r .venv
-python3.9 -m venv .venv
-source .venv/bin/activate
+env_path=$root_path/.venv
+
+rm -r $env_path
+python3.9 -m venv $env_path
+source $env_path/bin/activate
 
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
